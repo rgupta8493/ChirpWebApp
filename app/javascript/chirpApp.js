f01a05f9865b0b9e2d790d2496a62792ccf49f8e
rgupta8493 <r.gupta8493@ufl.edu>
2016-11-19 18:43:05 -0500

    Register and login page

--- app/javascript/chirpApp.js
+++ app/javascript/chirpApp.js
@@ -15,3 +15,18 @@ app.controller('mainController',function($scope){
   
     
                });
+
+app.controller('authController', function($scope){
+  $scope.user = {username: '', password: ''};
+  $scope.error_message = '';
+
+  $scope.login = function(){
+    //placeholder until authentication is implemented
+    $scope.error_message = 'login request for ' + $scope.user.username;
+  };
+
+  $scope.register = function(){
+    //placeholder until authentication is implemented
+    $scope.error_message = 'registeration request for ' + $scope.user.username;
+  };
+});
